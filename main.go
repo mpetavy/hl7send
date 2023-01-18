46c25d83c8df5de97af5f51934bcfe95fdf8c900
mpetavy <mpetavy>
2023-01-18 13:52:55 +0100

    common@v1.5.17 update, filemask support added

--- main.go
+++ main.go
@@ -12,24 +12,72 @@ import (
 )
 
 var (
-	conn        = flag.String("c", "localhost:9090", "<connection:port> to send to")
+	conn        = flag.String("c", "", "<connection:port> to send to")
 	filename    = flag.String("f", "", "file to send")
 	readtimeout = flag.Int("rt", 3000, "timeout in seconds for reading ACK")
 	looptimeout = flag.Int("lt", 1000, "timeout in seconds to wait between loops")
-	loop        = flag.Int("l", 1, "count loop")
+	loopCount   = flag.Int("lc", 1, "count loop")
 	plain       = flag.Bool("p", false, "no MLLP framing, just send")
 	useTls      = flag.Bool("tls", false, "Use TLS")
+	useACK      = flag.Bool("ack", false, "Use ACK")
 )
 
 func init() {
 	common.Init(false, "1.0.4", "", "", "2018", "Send HL7/TXT files", "mpetavy", fmt.Sprintf("https://github.com/mpetavy/%s", common.Title()), common.APACHE, nil, nil, nil, run, 0)
 }
 
-func run() error {
-	if !common.FileExists(*filename) {
-		return &common.ErrFileNotFound{*filename}
+func send(connection common.EndpointConnection, filename string) error {
+	sendBuffer, err := os.ReadFile(filename)
+	if err != nil {
+		return err
 	}
 
+	if !*plain {
+		common.Debug("Add MLLP framing")
+
+		sendBuf := bytes.Buffer{}
+		sendBuf.Write([]byte{0xb})
+		sendBuf.Write(sendBuffer)
+		sendBuf.Write([]byte{0x1c, 0xd})
+
+		sendBuffer = sendBuf.Bytes()
+	}
+
+	common.Debug("send bytes")
+	n, err := connection.Write(sendBuffer)
+	if err != nil {
+		return err
+	}
+	common.Debug("File: %s bytes written: %d", filename, n)
+
+	if *useACK {
+		if *readtimeout > 0 {
+			common.Debug("SetReadDeadline: %v", *readtimeout)
+
+			err := connection.SetReadDeadline(time.Now().Add(common.MillisecondToDuration(*readtimeout)))
+			if err != nil {
+				return err
+			}
+		}
+
+		receiveBuffer := make([]byte, 1024*1024)
+
+		n, err = connection.Read(receiveBuffer)
+		if err != nil {
+			return err
+		}
+
+		if !*plain {
+			receiveBuffer = receiveBuffer[1 : n-2]
+		}
+
+		fmt.Printf("%+v\n", string(receiveBuffer))
+	}
+
+	return nil
+}
+
+func run() error {
 	var tlsConfig *tls.Config
 	var err error
 
@@ -63,65 +111,35 @@ func run() error {
 		common.DebugError(connection.Close())
 	}()
 
-	for c := 0; c < *loop; c++ {
-		if *loop > 1 {
-			fmt.Printf("Loop: #%d\n", c)
-		}
+	for c := 0; c < *loopCount; c++ {
+		fw, err := common.NewFilewalker(*filename, true, false, func(path string, f os.FileInfo) error {
+			if f.IsDir() {
+				return nil
+			}
 
-		common.Debug("Read bytes")
-		sendBuffer, err := os.ReadFile(*filename)
-		if err != nil {
-			return err
-		}
+			if *loopCount > 1 {
+				fmt.Printf("Loop: #%d: %s\n", c, path)
+			}
 
-		if !*plain {
-			common.Debug("Add MLLP framing")
-			sendBuf := bytes.Buffer{}
-			sendBuf.Write([]byte{0xb})
-			sendBuf.Write(sendBuffer)
-			sendBuf.Write([]byte{0x1c, 0xd})
-
-			sendBuffer = sendBuf.Bytes()
-		}
-
-		common.Debug("send bytes")
-		n, err := connection.Write(sendBuffer)
-		if err != nil {
-			return err
-		}
-		common.Debug("Bytes written: %d", n)
-
-		if *readtimeout > 0 {
-			common.Debug("SetReadDeadline: %v", *readtimeout)
-			err := connection.SetReadDeadline(time.Now().Add(common.MillisecondToDuration(*readtimeout)))
-			if err != nil {
+			err := send(connection, path)
+			if common.Error(err) {
 				return err
 			}
 
-			receiveBuffer := make([]byte, 1024*1024)
-
-			n, err = connection.Read(receiveBuffer)
-			if err != nil {
-				return err
+			if *loopCount > 1 && (c+1) < *loopCount {
+				time.Sleep(time.Millisecond * time.Duration(*looptimeout))
 			}
 
-			if !*plain {
-				receiveBuffer = receiveBuffer[1 : n-2]
-			}
-
-			s := string(receiveBuffer)
-			s = common.ConvertToOSspecificLF(s)
-
-			fmt.Printf("%s\n", s)
+			return nil
+		})
+		if common.Error(err) {
+			return err
 		}
 
-		if *loop > 1 && (c+1) < *loop {
-			time.Sleep(time.Millisecond * time.Duration(*looptimeout))
-
-			continue
+		err = fw.Run()
+		if common.Error(err) {
+			return err
 		}
-
-		break
 	}
 
 	return nil
